e56b22abbacbe29d472727905188786b6028df67
LopezSanzJusto <justolopezsanz@gmail.com>
2026-05-25 17:50:44 +0200

    imagen de inicio Explora el Mundo... por fin ocupa toda la pantalla y esta bien metida

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -64,7 +64,7 @@ export default function WelcomeScreen() {
       <Image
         source={require('../assets/images/welcome-bg.png')}
         style={StyleSheet.absoluteFill}
-        contentFit="cover"
+        contentFit="fill"
         contentPosition="center"
       />
 
